c2b9f849aa7668a043096d3e753ced4c0accabdd
Jirakid Aiadhet <167008371+TOEYJIRAKID@users.noreply.github.com>
2025-03-16 23:17:49 +0700

    contact section - frontend 1

--- FORNTEND/pages/api/contacts.js
+++ FORNTEND/pages/api/contacts.js
@@ -2,5 +2,35 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Contact } from "@/models/contact";
 
 export default async function handle(req, res) {
-   
+
+    // if authenticated, connect to mongoDB
+    await mongooseConnect();
+
+    const { method } = req;
+
+    if (method === 'POST') {
+        try {
+            const { fname, lname, email, company, phone, country, project, price, description } = req.body;
+
+            const contactDoc = await Contact.create({
+                fname,
+                lname,
+                email,
+                company,
+                phone,
+                country,
+                project,
+                price,
+                description
+            });
+
+            res.status(201).json(contactDoc); // res 201 for create success
+        } catch (error) {
+            console.error('error create contact', error);
+            res.status(500).json({ error: 'failed to create contact' });
+        }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
 }
