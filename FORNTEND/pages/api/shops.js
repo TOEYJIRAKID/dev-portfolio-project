f9399a8fa14d172a78bf85ec5f3dd6e7fc3a0b54
Jirakid Aiadhet <167008371+TOEYJIRAKID@users.noreply.github.com>
2025-03-16 20:30:13 +0700

    shop section - frontend 1

--- FORNTEND/pages/api/shops.js
+++ FORNTEND/pages/api/shops.js
@@ -3,5 +3,26 @@ import { Shop } from "@/models/Shop";
 
 export default async function handle(req, res) {
 
-    
+    // if authenticated, connect to mongoDB
+    await mongooseConnect();
+
+    const { method } = req;
+
+    if (method === 'GET') {
+        if (req.query?.id) {
+            // fetch a single shop by id
+            const shop = await Shop.findById(req.query.id);
+            res.json(shop);
+        } else if (req.query?.slug) {
+            // fetch shop by slug
+            const shopslug = await Shop.find({ slug: req.query.slug });
+            res.json(shopslug.reverse());
+        } else {
+            // fetch all shop
+            const shops = await Shop.find();
+            res.json(shops.reverse());
+        }
+    } else {
+        res.status(405).json({ message: 'Method not allowed' });
+    }
 }
--- FORNTEND/pages/shop/index.js
+++ FORNTEND/pages/shop/index.js
@@ -1,8 +1,34 @@
 import Head from "next/head";
+import Link from "next/link";
+import Spinner from "@/components/Spinner";
+import { useState, useEffect } from "react";
+import useFetchData from '@/hooks/useFetchData';
 
 export default function shop() {
 
-    
+    // fetch blog data
+    const { alldata, loading } = useFetchData('/api/shops');
+
+    const publisheddata = alldata.filter(ab => ab.status === 'publish');
+
+    const createdAtDate = alldata && alldata[0]?.createdAt ? new Date(alldata && alldata[0]?.createdAt) : null;
+
+    // function to format the date as '23 may 2025 10:10 pm'
+    const formatDate = (date) => {
+        // check if date if valid
+        if (!date || isNaN(date)) {
+            return ''; // or handle the error as needed
+        }
+
+        const options = {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+            hour12: true // use 12-hour format
+        };
+
+        return new Intl.DateTimeFormat('en-US', options).format(date);
+    }
 
     return <>
 
@@ -10,7 +36,37 @@ export default function shop() {
             <title>Shop</title>
         </Head>
         <div className="shoppage">
-           
+            <div className="shoppagetoptitle">
+                <div className="container">
+                    <h3>SHOP ONLINE</h3>
+                    <h2>OUR PRODUCTS</h2>
+                </div>
+            </div>
+            <div className="shopproducts">
+                <div className="container">
+                    <div className="shopprocards">
+                        {loading ? <Spinner /> : <>
+                            {publisheddata.map((pro) => {
+                                return <Link href={`/shop/${pro.slug}`} key={pro._id} className="spprocard">
+                                    <div className="spprocardimg">
+                                        <img src={pro.images[0]} alt={pro.title} />
+                                    </div>
+                                    <div className="spprocinfo">
+                                        <h2>{pro.title}</h2>
+                                        <h3>$ {pro.price}</h3>
+                                        <div className="spprotags">
+                                            {pro.tags.map((tag) => {
+                                                return <span key={tag}>{tag}</span>
+                                            })}
+                                        </div>
+                                        <p>{formatDate(createdAtDate)}</p>
+                                    </div>
+                                </Link>
+                            })}
+                        </>}
+                    </div>
+                </div>
+            </div>
         </div>
     </>
 }
