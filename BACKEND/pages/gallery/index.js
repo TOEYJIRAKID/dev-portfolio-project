6b00ede00ad422f52f723f2a1c4ac024216d75df
Jirakid Aiadhet <167008371+TOEYJIRAKID@users.noreply.github.com>
2025-03-22 19:39:29 +0700

    add delete contact

--- BACKEND/pages/gallery/index.js
+++ BACKEND/pages/gallery/index.js
@@ -8,120 +8,165 @@ import Dataloading from "@/components/Dataloading";
 import LoginLayout from "@/components/LoginLayout";
 
 export default function gallery() {
+  // pagination
+  const [currentPage, setCurrentPage] = useState(1); // for page 1
+  const [perPage] = useState(5); // item per page
 
-    // pagination
-    const [currentPage, setCurrentPage] = useState(1); // for page 1
-    const [perPage] = useState(7);
+  // search
+  const [searchQuery, setSearchQuery] = useState("");
 
-    // search
-    const [searchQuery, setSearchQuery] = useState('');
+  // fetch blog data
+  const { alldata, loading } = useFetchData("/api/photos");
 
-    // fetch blog data
-    const { alldata, loading } = useFetchData('/api/photos');
+  // function to handle page change
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    // function to handle page change
-    const paginate = (pageNumber) => {
-        setCurrentPage(pageNumber)
-    };
+  // total number of blogs
+  const allblog = alldata.length;
 
-    // total number of blogs
-    const allblog = alldata.length;
+  // filter all data based on search query
+  const filteredBlogs =
+    searchQuery.trim() === ""
+      ? alldata
+      : alldata.filter((blog) =>
+          blog.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
 
-    // filter all data based on search query
-    const filteredBlogs = searchQuery.trim() === '' ? alldata : alldata.filter(blog => blog.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  // calculate index of the first blog displayed on the current page
+  const indexOfFirstBlog = (currentPage - 1) * perPage;
+  const indexOfLastBlog = currentPage * perPage;
 
-    // calculate index of the first blog displayed on the current page
-    const indexOfFirstBlog = (currentPage - 1) * perPage;
-    const indexOfLastBlog = currentPage * perPage;
+  // Get the current pag's blogs
+  const currentBlogs = filteredBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
+  const publishedblogs = currentBlogs;
+  const pageNumbers = [];
 
-    // Get the current pag's blogs
-    const currentBlogs = filteredBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
-    const publishedblogs = currentBlogs;
-    const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(allblog / perPage); i++) {
+    pageNumbers.push(i);
+  }
 
-    for (let i = 1; i <= Math.ceil(allblog / perPage); i++) {
-        pageNumbers.push(i);
-    }
-
-    return <>
-        <LoginLayout>
-            <div className="blogpage">
-                <div className="titledashboard flex flex-sb">
-                    <div>
-                        <h2>All Published <span>Photos</span></h2>
-                        <h3>ADMIN PANEL</h3>
-                    </div>
-                    <div className="breadcrumb">
-                        <SiBloglovin /><span>/</span><span>Photos</span>
-                    </div>
-                </div>
-                <div className="blogstable">
-                    <div className="flex gap-2 mb-1">
-                        <h2>Search Photos:</h2>
-                        <input value={searchQuery} onChange={ev => setSearchQuery(ev.target.value)} type="text" placeholder="Search by title..." />
-                    </div>
-                    <table className="table table-styling">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Image</th>
-                                <th>Title</th>
-                                <th>Edit / Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {loading ? <>
-                                <tr>
-                                    <td>
-                                        <Dataloading />
-                                    </td>
-                                </tr>
-                            </> : <>
-                                {publishedblogs.length === 0 ? (
-                                    <tr>
-                                        <td colSpan={4} className="text-center">No Photos Found</td>
-                                    </tr>
-                                ) : (
-                                    publishedblogs.map((blog, index) => (
-                                        <tr key={blog._id}>
-                                            <td>{indexOfFirstBlog + index + 1}</td>
-                                            <td><img src={blog.images[0]} width={180} alt="image" /></td>
-                                            <td><h3>{blog.title}</h3></td>
-                                            <td>
-                                                <div className="flex gap-2 flex-center">
-                                                    <Link href={'/gallery/edit/' + blog._id}>
-                                                        <button>
-                                                            <FaEdit />
-                                                        </button>
-                                                    </Link>
-                                                    <Link href={'/gallery/delete/' + blog._id}>
-                                                        <button>
-                                                            <RiDeleteBin6Fill />
-                                                        </button>
-                                                    </Link>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    ))
-                                )}
-                            </>}
-                        </tbody>
-                    </table>
-                    {/* for pagination */}
-                    {publishedblogs.length === 0 ? ("") : (
-                        <div className="blogpagination">
-                            <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
-                            {pageNumbers.slice(Math.max(currentPage - 3, 0), Math.min(currentPage + 2, pageNumbers.length)).map(number => (
-                                <button key={number} onClick={() => paginate(number)} className={`${currentPage === number ? 'active' : ''}`}>
-                                    {number}
-                                </button>
-                            ))}
-                            <button onClick={() => paginate(currentPage + 1)} disabled={currentBlogs.length < perPage}>Next</button>
-                        </div>
-                    )}
-                </div>
+  return (
+    <>
+      <LoginLayout>
+        <div className="blogpage">
+          <div className="titledashboard flex flex-sb">
+            <div>
+              <h2>
+                All Published <span>Photos</span>
+              </h2>
+              <h3>ADMIN PANEL</h3>
             </div>
-        </LoginLayout>
+            <div className="breadcrumb">
+              <SiBloglovin />
+              <span>/</span>
+              <span>Photos</span>
+            </div>
+          </div>
+          <div className="blogstable">
+            <div className="flex gap-2 mb-1">
+              <h2>Search Photos:</h2>
+              <input
+                value={searchQuery}
+                onChange={(ev) => setSearchQuery(ev.target.value)}
+                type="text"
+                placeholder="Search by title..."
+              />
+            </div>
+            <table className="table table-styling">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Edit / Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {loading ? (
+                  <>
+                    <tr>
+                      <td>
+                        <Dataloading />
+                      </td>
+                    </tr>
+                  </>
+                ) : (
+                  <>
+                    {publishedblogs.length === 0 ? (
+                      <tr>
+                        <td colSpan={4} className="text-center">
+                          No Photos Found
+                        </td>
+                      </tr>
+                    ) : (
+                      publishedblogs.map((blog, index) => (
+                        <tr key={blog._id}>
+                          <td>{indexOfFirstBlog + index + 1}</td>
+                          <td>
+                            <img src={blog.images[0]} width={180} alt="image" />
+                          </td>
+                          <td>
+                            <h3>{blog.title}</h3>
+                          </td>
+                          <td>
+                            <div className="flex gap-2 flex-center">
+                              <Link href={"/gallery/edit/" + blog._id}>
+                                <button>
+                                  <FaEdit />
+                                </button>
+                              </Link>
+                              <Link href={"/gallery/delete/" + blog._id}>
+                                <button>
+                                  <RiDeleteBin6Fill />
+                                </button>
+                              </Link>
+                            </div>
+                          </td>
+                        </tr>
+                      ))
+                    )}
+                  </>
+                )}
+              </tbody>
+            </table>
+            {/* for pagination */}
+            {publishedblogs.length === 0 ? (
+              ""
+            ) : (
+              <div className="blogpagination">
+                <button
+                  onClick={() => paginate(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  Previous
+                </button>
+                {pageNumbers
+                  .slice(
+                    Math.max(currentPage - 3, 0),
+                    Math.min(currentPage + 2, pageNumbers.length)
+                  )
+                  .map((number) => (
+                    <button
+                      key={number}
+                      onClick={() => paginate(number)}
+                      className={`${currentPage === number ? "active" : ""}`}
+                    >
+                      {number}
+                    </button>
+                  ))}
+                <button
+                  onClick={() => paginate(currentPage + 1)}
+                  disabled={currentBlogs.length < perPage}
+                >
+                  Next
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </LoginLayout>
     </>
-
+  );
 }
