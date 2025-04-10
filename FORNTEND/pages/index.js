371885e29b6396983cd19c4032a0414cf49f4440
Jirakid Aiadhet <167008371+TOEYJIRAKID@users.noreply.github.com>
2025-04-10 13:25:44 +0700

    update resume link

--- FORNTEND/pages/index.js
+++ FORNTEND/pages/index.js
@@ -160,7 +160,7 @@ export default function Home() {
               </div>
               <div className="hero_btn_box" data-aos="fade-up">
                 <Link
-                  href="https://drive.google.com/file/d/18j3Hwtiq9fTYEGQX0R1zx68G8T--reXj/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1A4qTJbI29Okn6Qa16ASxDa0XWOqH0Kg3/view?usp=sharing"
                   target="_blank"
                   className="download_cv"
                 >
